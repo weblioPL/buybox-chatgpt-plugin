f022ee54c690c67586d886bc6e790f35a543eefc
weblioPL <hello@weblio.pl>
2025-07-18 21:04:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.json());
 
